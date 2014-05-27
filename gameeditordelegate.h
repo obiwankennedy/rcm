c36f898e65e074f34e3cd61628b1159683a49d46
Renaud G. <renaud@rolisteam.org>
2014-05-27 15:32:28 +0200

    Update gameeditordelegate.h
    -fix issue about size

--- gameeditordelegate.h
+++ gameeditordelegate.h
@@ -29,11 +29,12 @@ public:
     GameEditorDelegate(QMap<QString,Game*>& l);
 
     virtual QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index	) const;
-    void setEditorData(QWidget *editor	, const QModelIndex &index);
+    void setEditorData(QWidget *editor	, const QModelIndex &index) const;
     void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
 
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    QSize sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const;
 private:
     QMap<QString,Game*> m_list;
 

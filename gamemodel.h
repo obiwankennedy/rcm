e241ae8855b473e26819db8e2ed446b1acc2c1b7
Renaud G. <renaud@rolisteam.org>
2014-06-06 18:14:48 +0200

    Update gamemodel.h
    xml

--- gamemodel.h
+++ gamemodel.h
@@ -42,7 +42,7 @@ public:
     virtual void readFromData(QDataStream&);
     virtual void writeToData(QDataStream&) const;
 
-    virtual QDomElement& writeDataToXml(QDomDocument&);
+    virtual QDomElement writeDataToXml(QDomDocument&);
     virtual void readDataFromXml(QDomDocument&);
 
 

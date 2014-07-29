9867f829c7ac6c94e32ea93f807f8fba2c5e4be8
Renaud Guezennec <renaud@buf.com>
2014-07-29 11:29:12 +0200

    Implementation of roommodel class

--- localisation/roommodel.cpp
+++ localisation/roommodel.cpp
@@ -1,6 +1,62 @@
 #include "roommodel.h"
 
-RoomModel::RoomModel(QObject *parent) :
+RoomModel::RoomModel(int roomCount,QObject *parent) :
     QAbstractListModel(parent)
 {
+    m_roomMap = new QMap<Qstring,int>();
+    for(int i = 0; i<roomCount; ++i)
+    {
+        m_roomMap->insert(tr("Room %1").arg(i),0);
+    }
+}
+void RoomModel::data(const QModelIndex &index, int role) const
+{
+    if(!index.isValid())
+        return QVariant();
+
+    if(m_roomMap->isEmpty())
+        return QVariant();
+
+    if(Qt::EditRole == role || Qt::DisplayRole == role)
+    {
+        QString key = m_roomMap->keys()[index.row()];
+        if(index.column()==0)
+        {
+            return key;
+        }
+        else
+        {
+            return m_roomMap->value(key);
+        }
+    }
+
+    return QVariant();
+
+}
+void RoomModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    QString key = m_roomMap->keys()[index.row()];
+    if(Qt::EditRole == role || Qt::DisplayRole == role)
+    {
+        if(index.column()==0)
+        {
+            int valueOld=m_roomMap->value(key);
+            m_roomMap->remove(key);
+            m_roomMap->insert(value.toString(),valueOld);
+
+        }
+        else
+        {
+            m_roomMap->insert(key,value.toInt());
+        }
+    }
+}
+
+void RoomModel::rowCount(const QModelIndex &parent) const
+{
+    return m_roomMap->size();
+}
+void RoomModel::columnCount(const QModelIndex &parent) const
+{
+    return 2;
 }
--- localisation/roommodel.h
+++ localisation/roommodel.h
@@ -2,17 +2,26 @@
 #define ROOMMODEL_H
 
 #include <QAbstractListModel>
+#include <QString>
+#include <QMap>
 
 class RoomModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
-    explicit RoomModel(QObject *parent = 0);
+    explicit RoomModel(int roomCount, QObject *parent = 0);
+
+    void data(const QModelIndex &index, int role) const;
+    void setData(const QModelIndex &index, const QVariant &value, int role);
+    void rowCount(const QModelIndex &parent) const;
+    void columnCount(const QModelIndex &parent) const;
 
 signals:
 
 public slots:
 
+private:
+    QMap<QString,int>* m_roomMap;
 };
 
 #endif // ROOMMODEL_H

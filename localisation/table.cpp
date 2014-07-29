4aca4557310265b446ac9fec60dab07d390dfb69
Renaud Guezennec <renaud@buf.com>
2014-07-29 10:51:53 +0200

    implementation of table class

--- localisation/table.cpp
+++ localisation/table.cpp
@@ -4,59 +4,63 @@ Table::Table()
 {
 
 }
-void Table::setRoomName(QString)
+void Table::setRoomName(QString str)
 {
-
+    m_id = str;
 }
 
-void Table::setDescription(QString)
+void Table::setDescription(QString str)
 {
-
+    m_description=str;
 }
 
-void Table::setId(int)
+void Table::setId(int id)
 {
-
+    m_id=id;
 }
 
-void Table::setCapacity()
+void Table::setCapacity(int c)
 {
-
+    m_capacity=c;
 }
 
-void Table::setState( bool)
+void Table::setState( bool avail)
 {
-
+    if(avail)
+        m_state=Table::AVAILABLE;
+    else
+        m_state=Table::BUSY;
 }
 
 
 
-bool Table::checkCapacity(int)
+bool Table::checkCapacity(int a)
 {
-
+    return (a <= m_capacity);
 }
 
 QString Table::getRoomName()
 {
-
+    return m_roomName;
 }
 
 QString Table::getDescription()
 {
+    return m_description;
 
 }
 
 int Table::getId()
 {
-
+    return m_id;
 }
 
 int Table::getCapacity()
 {
-
+    return m_capacity;
 }
 
-State Table::getCurrentState()
+Table::State Table::getCurrentState()
 {
-
+    return m_state;
 }

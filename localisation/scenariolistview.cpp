2dd95f6da711d347853e69ab6389c3ce3586e588
rguezennec <renaud@rolisteam.org>
2017-08-31 17:21:26 +0200

    -Add new files to manage drag and drop scenario on schedules.

---
+++ localisation/scenariolistview.cpp
@@ -0,0 +1,6 @@
+#include "scenariolistview.h"
+
+ScenarioListView::ScenarioListView()
+{
+
+}
---
+++ localisation/scenariolistview.h
@@ -0,0 +1,12 @@
+#ifndef SCENARIOLISTVIEW_H
+#define SCENARIOLISTVIEW_H
+
+#include <QWidget>
+
+class ScenarioListView : public QListView
+{
+public:
+    ScenarioListView();
+};
+
+#endif // SCENARIOLISTVIEW_H

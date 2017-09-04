cf1dd97c12331e400dd8b073c904e6ded5cc56ce
Renaud G <renaud@rolisteam.org>
2017-09-04 10:08:39 +0200

    display scenario information properly -manage lock/unlock state -manage incoming/running/done state

--- localisation/scenarioitem.cpp
+++ localisation/scenarioitem.cpp
@@ -1,36 +1,196 @@
 #include "scenarioitem.h"
 #include <QPainter>
 
+
+#include <QGraphicsPixmapItem>
+
 ScenarioItem::ScenarioItem()
 {
+    m_lockUp = false;
+    scheduleGame();
+    setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable | QGraphicsItem::ItemSendsGeometryChanges);
+
+    m_idTranslator = IdTranslator::getInstance();
+
+
+
+    m_locker = new QGraphicsPixmapItem(this);
+
+    m_locker->setPixmap(QPixmap(":/resources/cadenas_open.png"));
+
+
+
 
 }
 
 QRectF ScenarioItem::boundingRect() const
 {
+
     return QRectF(0,0,(*m_minutesInPixel)*m_data->getDuration(),*m_tableInPixel);
 }
-
+#include <QStyleOptionGraphicsItem>
 void ScenarioItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = boundingRect();
 
-    painter->fillRect(rect,Qt::white);
+    if(nullptr != m_idTranslator)
+    {
 
-    painter->setPen(Qt::black);
-    painter->drawRect(rect);
+        Game* game = m_idTranslator->getGameById(m_data->getGameId());
+        GameMaster* master = m_idTranslator->getGameMasterById(m_data->getGameMasterId());
 
-    painter->drawText(rect.center().x(),rect.topLeft().y(),m_data->getTitle());
-    painter->drawText(rect.center(),m_data->getTitle());
+        if(nullptr != master)
+        {
+            QColor color = master->getColor();
+            if(Done==m_state)
+            {
+                color = QColor(80,80,80);
+            }
 
 
-    painter->drawText(rect.bottomRight(),QString("%1/%2").arg(m_data->getCurrentPlayers(),m_data->getMaximumPlayers()));
+            painter->fillRect(rect,color);
+        }
+        m_locker->setPos(rect.right()*0.8,rect.center().y()-m_locker->boundingRect().height()/2);
+
+        painter->setPen(Qt::black);
+        painter->drawRect(rect);
+
+        if(option->state & QStyle::State_Selected )
+        {
+            painter->save();
+            painter->setPen(Qt::red);
+            painter->drawRect(rect);
+            painter->restore();
+        }
 
 
+        if((game != nullptr)&&(nullptr != master))
+        {
+            painter->drawText(rect.center().x(),rect.topLeft().y(),m_data->getTitle());
+            painter->drawText(0,rect.center().y(),game->getTitle());
+            painter->drawText(rect,Qt::AlignTop | Qt::AlignLeft,tr("GM: %1 - tel: %2").arg(master->getNickName()).arg(master->getPhoneNumber()));
+            painter->drawText(rect,Qt::AlignBottom | Qt::AlignRight,QString("%1 min").arg(m_data->getDuration()));
+            painter->drawText(0,rect.bottom(),QString("%1/%2").arg(m_data->getCurrentPlayers()).arg(m_data->getMaximumPlayers()));
+            painter->drawText(rect.topRight(),tr("State: %1").arg(stateToString()));
 
+        }
+    }
+
+}
+#include "localisationview.h"
+QVariant ScenarioItem::itemChange(GraphicsItemChange change, const QVariant &value)
+{
+
+    if (change == ItemPositionChange )
+    {
+
+        auto schedule = dynamic_cast<Schedules*>(scene());
+
+        QPointF newPos = schedule->computeClosePoint(value.toPointF());
+        if(newPos != value.toPointF())
+        {
+            return newPos;
+        }
+    }
+
+    return QGraphicsItem::itemChange(change, value);
+}
+
+void ScenarioItem::readFromData(QDataStream &)
+{
 
 }
 
+void ScenarioItem::writeToData(QDataStream & in) const
+{
+    in << m_color;
+    in << m_state;
+    m_data->writeToData(in);
+
+}
+
+void ScenarioItem::readDataToJson(QJsonObject &)
+{
+
+}
+
+void ScenarioItem::writeDataToJson(QJsonObject &)
+{
+
+}
+
+QDomElement ScenarioItem::writeDataToXml(QDomDocument &)
+{
+
+}
+
+void ScenarioItem::readDataFromXml(QDomNode &)
+{
+
+}
+QString ScenarioItem::stateToString()
+{
+    switch(m_state)
+    {
+    case Done:
+        return QStringLiteral("Done");
+    case Running:
+        return QStringLiteral("Running");
+    case Scheduled:
+        return QStringLiteral("Scheduled");
+    }
+    return "";
+}
+
+void ScenarioItem::startGame()
+{
+    m_state = Running;
+    update();
+}
+
+void ScenarioItem::endGame()
+{
+    m_state = Done;
+    update();
+}
+
+void ScenarioItem::scheduleGame()
+{
+    m_state = Scheduled;
+    update();
+}
+
+void ScenarioItem::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        auto children = childItems();
+        QPointF position = event->scenePos();
+        for(auto child : children)
+        {
+            QPointF newPos = child->mapFromScene(position);
+            if(child->contains(newPos))
+            {
+                if(child == m_locker)
+                {
+                    m_lockUp=!m_lockUp;
+                    if(m_lockUp)
+                    {
+                        m_locker->setPixmap(QPixmap(":/resources/cadenas_closed.png"));
+                        setFlags( QGraphicsItem::ItemIsSelectable);
+                        update();
+                    }
+                    else
+                    {
+                        m_locker->setPixmap(QPixmap(":/resources/cadenas_open.png"));
+                        setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable | QGraphicsItem::ItemSendsGeometryChanges);
+                        update();
+                    }
+                }
+            }
+        }
+    }
+}
 qreal *ScenarioItem::minutesInPixel() const
 {
     return m_minutesInPixel;
@@ -40,6 +200,7 @@ void ScenarioItem::setMinutesInPixel(qreal* minutesInPixel)
 {
     qDebug() << "ScenarioItem minute in pixel"<< minutesInPixel;
     m_minutesInPixel = minutesInPixel;
+    update();
 }
 
 Scenario *ScenarioItem::data() const
@@ -60,4 +221,5 @@ qreal *ScenarioItem::tableInPixel() const
 void ScenarioItem::setTableInPixel(qreal *tableInPixel)
 {
     m_tableInPixel = tableInPixel;
+    update();
 }
--- localisation/scenarioitem.h
+++ localisation/scenarioitem.h
@@ -2,12 +2,19 @@
 #define SCENARIOITEM_H
 
 #include <QGraphicsObject>
+#include <QGraphicsSceneMouseEvent>
 
 #include "scenario.h"
-
-class ScenarioItem : public QGraphicsObject
+#include "idtranslator.h"
+#include "serializable.h"
+/**
+ * @brief The ScenarioItem class
+ */
+class ScenarioItem : public QGraphicsObject, public Serialisable
 {
+    Q_OBJECT
 public:
+    enum State {Scheduled, Running, Done};
     explicit ScenarioItem();
 
     QRectF boundingRect() const;
@@ -22,10 +29,37 @@ public:
     qreal *tableInPixel() const;
     void setTableInPixel(qreal *tableInPixel);
 
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+
+    virtual void readFromData(QDataStream&);
+    virtual void writeToData(QDataStream&) const;
+
+    virtual void readDataToJson(QJsonObject&);
+    virtual void writeDataToJson(QJsonObject&);
+
+    virtual QDomElement writeDataToXml(QDomDocument&);
+    virtual void readDataFromXml(QDomNode&);
+
+public slots:
+    void startGame();
+    void endGame();
+    void scheduleGame();
+
+signals:
+    void positionChanged(Scenario*);
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *);
+
+    QString stateToString();
 private:
     Scenario* m_data;
     qreal* m_minutesInPixel;
     qreal* m_tableInPixel;
+    IdTranslator* m_idTranslator;
+    QColor m_color;
+    bool m_lockUp;
+    QGraphicsPixmapItem* m_locker;
+    State m_state;
 };
 
 #endif // SCENARIOITEM_H

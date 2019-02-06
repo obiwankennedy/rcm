0b3dfc2ee653592e9dadf07275e163f5c347cc41
Renaud G <renaud@rolisteam.org>
2019-02-06 10:33:07 +0100

    new file for rcm

---
+++ definesize.h
@@ -0,0 +1,4 @@
+#ifndef DEFINESIZE_H
+#define DEFINESIZE_H
+
+#endif // DEFINESIZE_H
---
+++ preferences/preferencesdialog.h
@@ -0,0 +1,22 @@
+#ifndef PREFERENCESDIALOG_H
+#define PREFERENCESDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class PreferencesDialog;
+}
+
+class PreferencesDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PreferencesDialog(QWidget *parent = nullptr);
+    ~PreferencesDialog();
+
+private:
+    Ui::PreferencesDialog *ui;
+};
+
+#endif // PREFERENCESDIALOG_H
